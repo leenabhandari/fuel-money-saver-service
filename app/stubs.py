666ba838ac30c9a6371298b32e3227a0f1e123b8
Leena <leenabhandari1@gmail.com>
2020-12-20 16:59:48 +0530

    added multiplier inside the algorithm

--- app/stubs.py
+++ app/stubs.py
@@ -3,37 +3,43 @@ pumps = [{
         'lat':2.3386,
         'lon':8.6634,
         'cost':20.4,
-        'distToNext':100
+        'distToNext':100,
+        'multiplier':1.1
     },
     {
         'lat':2.3386,
         'lon':8.6634,
         'cost':10.5,
-        'distToNext':100
+        'distToNext':100,
+        'multiplier':1
     },
     {
         'lat':2.3386,
         'lon':8.66341,
         'cost':30.2,
-        'distToNext':200
+        'distToNext':200,
+        'multiplier':1
     },
     {
         'lat':2.3386,
         'lon':8.66324,
         'cost':10.2,
-        'distToNext':200
+        'distToNext':200,
+        'multiplier':1
     },
     {
         'lat':2.3386,
         'lon':8.66384,
         'cost':10.2,
-        'distToNext':100
+        'distToNext':100,
+        'multiplier':1.1
     },
     {
         'lat':2.3386,
         'lon':8.66304,
         'cost':40.2,
-        'distToNext':0
+        'distToNext':0,
+        'multiplier':1
     },]
 
 avgFuelRate = 10
